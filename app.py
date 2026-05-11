1d08312d7a08f126ea409f7386e86a1e18c2e602
Rizqi2552 <rizqia2552@gmail.com>
2026-05-11 11:56:24 +0800

    fix attendance by nip

--- app.py
+++ app.py
@@ -109,11 +109,11 @@ def create_attendance(df, bulan, tahun, minggu="all"):
 
         tabel[col] = ""
 
-        hadir = df[df['tanggal'] == tgl.date()]['name'].unique()
+    hadir = df[df['tanggal'] == tgl.date()]['id number'].astype(str).str.strip().unique()
 
-        for i in tabel.index:
-            if tabel.loc[i, 'Nama'] in hadir:
-                tabel.loc[i, col] = "H"
+    for i in tabel.index:
+        if tabel.loc[i, 'NIP'] in hadir:
+            tabel.loc[i, col] = "H"
 
         if tgl.weekday() == 5:
             day_map[col] = "sabtu"
